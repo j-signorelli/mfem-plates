e72b9a6bab062897acc1f2e1ec575a02d07c0b0a
Joseph Signorelli <jms26@illinois.edu>
2025-10-28 11:44:43 -0500

    Include tri mesh + resolve boundary bug. Looks good!!

--- src/panel_kl.cpp
+++ src/panel_kl.cpp
@@ -32,7 +32,10 @@ struct KL_Context
 
 
    // Penalty coefficient
-   double eta = 1e5;
+   double eta = 10;
+
+   bool quads = false;
+
 } ctx;
 
 class BiharmonicIntegrator : public BilinearFormIntegrator
@@ -51,19 +54,14 @@ public:
 
 class C0InteriorPenaltyIntegrator : public BilinearFormIntegrator
 {
+private:
    const double eta;
 
-   // AssembleBlock Helpers:
-   mutable Vector n_b, dnshape_a, dnshape_b, nd2nshape_b, nv;
-
    // AssembleFaceMatrix Helpers:
-   mutable Vector normal_1, normal_2;
-   mutable DenseMatrix dshape_1, dshape_2, hessian_1, hessian_2, block11, block12, block21, block22, elmat_p;
-
+   mutable Vector normal[2], dnshape[2], nv[2], nd2nshape[2];
+   mutable DenseMatrix dshape[2], hessian[2], blockJ[2][2], blockC[2][2], elmatJ_p, elmatC_p;
 public:
    C0InteriorPenaltyIntegrator(double eta_) : eta(eta_) {};
-
-   void AssembleBlock(const DenseMatrix &dshape_a, const DenseMatrix &dshape_b, const DenseMatrix &hessian_b, const Vector &n_a, const Vector &n_b, double h_e, DenseMatrix &elmat_ab);
 
    void AssembleFaceMatrix(const FiniteElement &el1, const FiniteElement &el2, FaceElementTransformations &Trans, DenseMatrix &elmat) override;
 };
@@ -104,7 +102,7 @@ int main(int argc, char *argv[])
    args.AddOption(&ctx.Nx, "-Nx", "--numelems-x", "Number of elements in panel x-direction.");
    args.AddOption(&ctx.Ny, "-Ny", "--numelems-y", "Number of elements in panel y-direction.");
 
-   args.AddOption(&ctx.rs, "-rs", "--refine-serial", "Number of times to refine mesh in serial.");
+   args.AddOption(&ctx.rs, "-rs", "--refinements", "Number of times to refine mesh.");
    args.AddOption(&ctx.order, "-o", "--order", "Order of the finite elements.");
 
    args.AddOption(&ctx.E, "-E", "--youngs-modulus", "Young's modulus of the panel.");
@@ -114,6 +112,7 @@ int main(int argc, char *argv[])
 
    args.AddOption(&ctx.eta, "-eta", "--penalty-coeff", "Penalty coefficient.");
 
+   args.AddOption(&ctx.quads, "-qd", "--quads", "-ti", "--tris", "Use quads or tris.");
 
    args.Parse();
    if (!args.Good())
@@ -127,104 +126,133 @@ int main(int argc, char *argv[])
    }
 
    // Generate panel mesh
-   Mesh m = Mesh::MakeCartesian2D(ctx.Nx, ctx.Ny, Element::Type::QUADRILATERAL, true, ctx.Lx, ctx.Ly);
+   Mesh m;
+   if (ctx.quads)
+   {
+      m = Mesh::MakeCartesian2D(ctx.Nx, ctx.Ny, Element::Type::QUADRILATERAL, true, ctx.Lx, ctx.Ly);
+   }
+   else
+   {
+      m = Mesh("tri_plate.mesh", 1, 0);
+   }
    m.SetCurvature(ctx.order); // ensure isoparametric!
    int dim = m.Dimension();
-
-   // Refine the mesh
-   for (int i = 0; i < ctx.rs; i++)
-   {
-      m.UniformRefinement();
-   }
 
    // Partition the mesh
    ParMesh pmesh(MPI_COMM_WORLD, m);
 
-   // Initialize the FE collection and FiniteElementSpace
-   H1_FECollection fe_coll(ctx.order, dim);
-   ParFiniteElementSpace fespace(&pmesh, &fe_coll, 1);
+   Array<int> ref;
+   Array<double> l2_error;
+   Array<double> h1_error;
+   // Refine the mesh
+   for (int i = 0; i < ctx.rs; i++)
+   {
 
-   // Get the degrees-of-freedom (DOFs) associated with the sides of the panel
-   // (these will be fixed/clamped)
-   Array<int> all_bdr_marker(pmesh.bdr_attributes.Size());
-   all_bdr_marker = 1; // Mark all sides
+      // Initialize the FE collection and FiniteElementSpace
+      H1_FECollection fe_coll(ctx.order, dim);
+      ParFiniteElementSpace fespace(&pmesh, &fe_coll, 1);
 
-   Array<int> ess_tdof_list;
-   fespace.GetEssentialTrueDofs(all_bdr_marker, ess_tdof_list);
-   // ess_tdof_list now holds indices of the DOFs associated with the panel sides
+      // Get the degrees-of-freedom (DOFs) associated with the sides of the panel
+      // (these will be fixed/clamped)
+      Array<int> all_bdr_marker(pmesh.bdr_attributes.Size());
+      all_bdr_marker = 1; // Mark all sides
 
-   // Create a coefficient representing the uniform pressure BC
-   //ConstantCoefficient p_load(-ctx.delta_p_uniform);
-   FunctionCoefficient p_load(dp_test);
+      Array<int> ess_tdof_list;
+      fespace.GetEssentialTrueDofs(all_bdr_marker, ess_tdof_list);
+      // ess_tdof_list now holds indices of the DOFs associated with the panel sides
 
-   // Compute bending stiffness D using E, nu, and t, as coefficient
-   double D_val = 1.0;//2*pow(ctx.t,3)*ctx.E/(3*(1-ctx.nu));
-   ConstantCoefficient D(D_val);
+      // Create a coefficient representing the uniform pressure BC
+      //ConstantCoefficient p_load(-ctx.delta_p_uniform);
+      FunctionCoefficient p_load(dp_test);
 
-   // Initialize the bilinear form representing the LHS (stiffness matrix K in Ku=f)
-   ParBilinearForm k(&fespace);
-   k.AddDomainIntegrator(new BiharmonicIntegrator(D));
-   k.AddInteriorFaceIntegrator(new C0InteriorPenaltyIntegrator(ctx.eta));
-   k.AddBdrFaceIntegrator(new C0InteriorPenaltyIntegrator(ctx.eta));
+      // Compute bending stiffness D using E, nu, and t, as coefficient
+      double D_val = 1.0;//2*pow(ctx.t,3)*ctx.E/(3*(1-ctx.nu));
+      ConstantCoefficient D(D_val);
 
-   // Initialize the linear form representing the LHS (forcing term f in Ku=f)
-   ParLinearForm f(&fespace);
-   f.AddDomainIntegrator(new DomainLFIntegrator(p_load));
+      // Initialize the bilinear form representing the LHS (stiffness matrix K in Ku=f)
+      ParBilinearForm k(&fespace);
+      k.AddDomainIntegrator(new BiharmonicIntegrator(D));
+      k.AddInteriorFaceIntegrator(new C0InteriorPenaltyIntegrator(ctx.eta));
+      k.AddBdrFaceIntegrator(new C0InteriorPenaltyIntegrator(ctx.eta));
 
-   // Initialize the solution
-   ParGridFunction W_gf(&fespace);
-   W_gf = 0.0;
-   W_gf.SetTrueVector();
+      // Initialize the linear form representing the LHS (forcing term f in Ku=f)
+      ParLinearForm f(&fespace);
+      f.AddDomainIntegrator(new DomainLFIntegrator(p_load));
 
-   // Assemble the stiffness matrix
-   k.Assemble();
-   k.Finalize();
-   std::unique_ptr<HypreParMatrix> K_mat(k.ParallelAssemble());
+      // Initialize the solution
+      ParGridFunction W_gf(&fespace);
+      W_gf = 0.0;
+      W_gf.SetTrueVector();
 
-   // Assemble the RHS
-   Vector F_vec(fespace.GetTrueVSize());
-   f.Assemble();
-   f.ParallelAssemble(F_vec);
+      // Assemble the stiffness matrix
+      k.Assemble();
+      k.Finalize();
+      std::unique_ptr<HypreParMatrix> K_mat(k.ParallelAssemble());
 
-   // Apply boundary conditions to the matrix system
-   std::unique_ptr<HypreParMatrix> K_e(K_mat->EliminateRowsCols(ess_tdof_list));
-   K_mat->EliminateBC(*K_e, ess_tdof_list, W_gf.GetTrueVector(), F_vec);
+      // Assemble the RHS
+      Vector F_vec(fespace.GetTrueVSize());
+      f.Assemble();
+      f.ParallelAssemble(F_vec);
 
-   // Initialize preconditioner
-   HypreBoomerAMG amg(*K_mat);
+      // Apply boundary conditions to the matrix system
+      std::unique_ptr<HypreParMatrix> K_e(K_mat->EliminateRowsCols(ess_tdof_list));
+      K_mat->EliminateBC(*K_e, ess_tdof_list, W_gf.GetTrueVector(), F_vec);
 
-   // Initialize solver
-   HyprePCG pcg(*K_mat);
-   pcg.SetTol(1e-8);
-   pcg.SetMaxIter(100000);
-   pcg.SetPrintLevel(2);
-   pcg.SetPreconditioner(amg);
-   
-   // Solve Ku=f
-   pcg.Mult(F_vec, W_gf.GetTrueVector());
-   W_gf.SetFromTrueVector();
+      // Initialize preconditioner
+      HypreBoomerAMG amg(*K_mat);
+      amg.SetPrintLevel(0);
 
-   // // Write the output
-   ParaViewDataCollection pvdc("KirchoffLove", &pmesh);
-   pvdc.SetHighOrderOutput(true);
-   pvdc.RegisterField("Deformation", &W_gf);
+      // Initialize solver
+      HyprePCG pcg(*K_mat);
+      pcg.SetTol(1e-8);
+      pcg.SetMaxIter(100000);
+      pcg.SetPrintLevel(0);
+      pcg.SetPreconditioner(amg);
+      
+      // Solve Ku=f
+      pcg.Mult(F_vec, W_gf.GetTrueVector());
+      W_gf.SetFromTrueVector();
 
-   FunctionCoefficient exact_coeff(w_exact);
-   VectorFunctionCoefficient exact_grad_coeff(2, grad_w_exact);
+      FunctionCoefficient exact_coeff(w_exact);
+      VectorFunctionCoefficient exact_grad_coeff(2, grad_w_exact);
 
-   ParGridFunction exact_gf(&fespace);
-   exact_gf.ProjectCoefficient(exact_coeff);
-   pvdc.RegisterField("Exact", &exact_gf);
-   pvdc.Save();
+      // // Write the output
+      // ParaViewDataCollection pvdc("KirchoffLove", &pmesh);
+      // pvdc.SetHighOrderOutput(true);
+      // pvdc.RegisterField("Deformation", &W_gf);
 
-   double err_L2 = W_gf.ComputeL2Error(exact_coeff);
-   double err_H1 = W_gf.ComputeH1Error(&exact_coeff, &exact_grad_coeff);
+      // ParGridFunction exact_gf(&fespace);
+      // exact_gf.ProjectCoefficient(exact_coeff);
+      // pvdc.RegisterField("Exact", &exact_gf);
+      // pvdc.Save();
+
+      double err_L2 = W_gf.ComputeL2Error(exact_coeff);
+      double err_H1 = W_gf.ComputeH1Error(&exact_coeff, &exact_grad_coeff);
+      if (rank == 0)
+      {
+         cout << "-------------------------------------------------------" << endl;
+         cout << "RS: " << i << endl;
+         int num_iterations; pcg.GetNumIterations(num_iterations);
+         cout << "Num Iterations: " << num_iterations << endl; 
+         cout << "L2 Error: " << err_L2 << endl;
+         cout << "H1 Error: " << err_H1 << endl;
+      }
+      
+      ref.Append(i);
+      l2_error.Append(err_L2);
+      h1_error.Append(err_H1);
+      pmesh.UniformRefinement();
+   }
    if (rank == 0)
    {
-      cout << "L2 Error: " << err_L2 << endl;
-      cout << "H1 Error: " << err_H1 << endl;
+      cout << "=======================================================" << endl;
+      cout << "SUMMARY:" << endl;
+      cout << "Eta: " << ctx.eta << endl;
+      cout << "Order: " << ctx.order << endl;
+      cout << "Refinements: "; ref.Print(cout, ctx.rs);
+      cout << "L2 Errors: "; l2_error.Print(cout, ctx.rs);
+      cout << "H1 Errors: "; h1_error.Print(cout, ctx.rs);
    }
-
    return 0;
 }
 
@@ -265,82 +293,49 @@ void BiharmonicIntegrator::AssembleElementMatrix(const FiniteElement &el, Elemen
    }
 }
 
-void C0InteriorPenaltyIntegrator::AssembleBlock(const DenseMatrix &dshape_a, const DenseMatrix &dshape_b, const DenseMatrix &hessian_b, const Vector &n_a, const Vector &n_b, double h_e, DenseMatrix &elmat_ab)
-{  
-   elmat_ab = 0.0;
-
-   dnshape_a.SetSize(dshape_a.NumRows()); // ndofs_a
-   dnshape_b.SetSize(dshape_b.NumRows()); // ndofs_b
-   nd2nshape_b.SetSize(hessian_b.NumRows()); // ndofs_b
-   nv.SetSize(hessian_b.NumCols());
-
-   // dshape_a = (dof, dim)
-   dshape_a.Mult(n_a, dnshape_a);
-
-   // dshape_b = (dof, dim)
-   dshape_b.Mult(n_b, dnshape_b);
-
-   // hessian_b = (dof, [3 in 2D])
-   nv[0] = pow(n_b[0],2);
-   nv[1] = 2*n_b[0]*n_b[1];
-   nv[2] = n_b[1]*n_b[1];
-
-   hessian_b.Mult(nv, nd2nshape_b);
-
-   // Consistency term:
-   AddMult_a_VWt(1.0, dnshape_a, nd2nshape_b, elmat_ab);
-
-   // Penalty term (symmetric):
-   AddMult_a_VWt(eta/h_e, dnshape_a, dnshape_b, elmat_ab);
-}
-
-/** Compute: q^(a,b) + p^(a,b)
-         
-         q^(a,b) = [d\phi^(a)/dn^(a)][d^2\phi^b/dn^(a)^2], or
-         q^(a,b) = [(grad \phi^a) dot n^(a)]*[n^(b)^T dot hess(\phi)^b dot n^(b)]
-
-         and
-
-         p^(a,b) = [d\phi^(a)/dn^(a)][d\phi^(b)/dn^(b)] or
-                 = [(grad \phi^a) dot n^(a)][(grad \phi^b) dot n^(b)]
-*/
 void C0InteriorPenaltyIntegrator::AssembleFaceMatrix(const FiniteElement &el1, const FiniteElement &el2, FaceElementTransformations &Trans, DenseMatrix &elmat)
 {
    int dim = el1.GetDim();
-   int ndof1 = el1.GetDof();
-   int ndof2 = 0;
    MFEM_ASSERT(dim == 2, "Dimension must be 2.");
 
-   // For boundary face integration:
+   int ndof[2] = {el1.GetDof(), 0};
+   int num_elems = 1;
    if (Trans.Elem2No >= 0)
    {
-      ndof2 = el2.GetDof();
+      ndof[1] = el2.GetDof();
+      num_elems++;
    }
 
-   normal_1.SetSize(dim);
-   dshape_1.SetSize(ndof1, dim);
-   hessian_1.SetSize(ndof1, dim * (dim + 1) / 2);
-   block11.SetSize(ndof1, ndof1);
-   if (ndof2 > 0)
+   for (int i = 0; i < num_elems; i++)
    {
-      dshape_2.SetSize(ndof2, dim);
-      normal_2.SetSize(dim);
-      hessian_2.SetSize(ndof2, dim * (dim + 1) / 2);
-      block12.SetSize(ndof1, ndof2);
-      block21.SetSize(ndof2, ndof1);
-      block22.SetSize(ndof2, ndof2);
+      normal[i].SetSize(dim);
+      dshape[i].SetSize(ndof[i], dim);
+      hessian[i].SetSize(ndof[i], dim * (dim + 1) / 2);
+      nv[i].SetSize(dim * (dim + 1) / 2);
+      dnshape[i].SetSize(ndof[i]);
+      nd2nshape[i].SetSize(ndof[i]);
    }
-   elmat.SetSize(ndof1 + ndof2);
-   elmat_p.SetSize(ndof1 + ndof2);
+
+   for (int i = 0; i < num_elems; i++)
+   {
+      for (int j = 0; j < num_elems; j++)
+      {
+         blockJ[i][j].SetSize(ndof[i], ndof[j]);
+         blockC[i][j].SetSize(ndof[i], ndof[j]);
+      }
+   }
+
+   elmatJ_p.SetSize(ndof[0] + ndof[1]);
+   elmatC_p.SetSize(ndof[0] + ndof[1]);
+   elmat.SetSize(ndof[0] + ndof[1]);
    elmat = 0.0;
 
    const IntegrationRule *ir = IntRule;
    if (ir == NULL)
    {
-      int order = 2 * max(el1.GetOrder(), ndof2 ? el2.GetOrder() : 0);
+      int order = 2 * max(el1.GetOrder(), ndof[1] ? el2.GetOrder() : 0);
       ir = &IntRules.Get(Trans.GetGeometryType(), order);
    }
-
 
    // Compute edge length
    double h_e  = 0.0;
@@ -351,49 +346,67 @@ void C0InteriorPenaltyIntegrator::AssembleFaceMatrix(const FiniteElement &el1, c
       h_e += ip.weight * Trans.Weight();
    }
 
+   const FiniteElement *els[2] = {&el1, &el2};
+   ElementTransformation *el_trans[2] = {Trans.Elem1, Trans.Elem2};
+
    for (int p = 0; p < ir->GetNPoints(); p++)
    {
+      elmatJ_p = 0.0;
+      elmatC_p = 0.0;
+
       const IntegrationPoint &ip = ir->IntPoint(p);
 
       // Set the integration point in the face and the neighboring elements
       Trans.SetAllIntPoints(&ip);
 
-      // Compute normals, derivatives, and hessians
-      CalcOrtho(Trans.Jacobian(), normal_1);
-      normal_1 /= normal_1.Norml2();
-      el1.CalcPhysDShape(*Trans.Elem1, dshape_1);
-      el1.CalcPhysHessian(*Trans.Elem1, hessian_1);
-      if (ndof2)
+      // Compute normal gradients + Hessians
+      for (int i = 0; i < num_elems; i++)
       {
-         normal_2 = normal_1;
-         normal_2 *= -1.0;
-         el2.CalcPhysDShape(*Trans.Elem2, dshape_2);
-         el2.CalcPhysHessian(*Trans.Elem2, hessian_2);
+         if (i == 0)
+         {
+            CalcOrtho(Trans.Jacobian(), normal[i]);
+            normal[i] /= normal[i].Norml2();
+         }
+         else
+         {
+            normal[i] = normal[0];
+            normal[i] *= -1;
+         }
+         els[i]->CalcPhysDShape(*el_trans[i], dshape[i]);
+         els[i]->CalcPhysHessian(*el_trans[i], hessian[i]);
+         dshape[i].Mult(normal[i], dnshape[i]);
+         nv[i][0] = normal[i][0]*normal[i][0];
+         nv[i][1] = 2*normal[i][0]*normal[i][1];
+         nv[i][2] = normal[i][1]*normal[i][1];
+         hessian[i].Mult(nv[i], nd2nshape[i]);
       }
 
-      // (1,1) block
-      AssembleBlock(dshape_1, dshape_1, hessian_1, normal_1, normal_1, h_e, block11);
-      elmat_p.SetSubMatrix(0, 0, block11);
-      if (ndof2 > 0)
+      // Compute blocks
+      for (int i = 0; i < num_elems; i++)
       {
-         // (1,2) block
-         AssembleBlock(dshape_1, dshape_2, hessian_2, normal_1, normal_2, h_e, block12);
-         elmat_p.SetSubMatrix(0, ndof1, block12);
-
-         // (2,1) block
-         AssembleBlock(dshape_2, dshape_1, hessian_1, normal_2, normal_1, h_e, block21);
-         elmat_p.SetSubMatrix(ndof1, 0, block21);
-
-         // (2,2) block
-         AssembleBlock(dshape_2, dshape_2, hessian_2, normal_2, normal_2, h_e, block22);
-         elmat_p.SetSubMatrix(ndof1, ndof1, block22);
+         for (int j = 0; j < num_elems; j++)
+         {  
+            blockJ[i][j] = 0.0;
+            blockC[i][j] = 0.0;
+            AddMult_a_VWt(1.0, dnshape[i], nd2nshape[j], blockJ[i][j]);
+            elmatJ_p.SetSubMatrix(i*ndof[0], j*ndof[0], blockJ[i][j]);
+            
+            AddMult_a_VWt(eta/h_e, dnshape[i], dnshape[j], blockC[i][j]);
+            elmatC_p.SetSubMatrix(i*ndof[0], j*ndof[0], blockC[i][j]);
+         }
       }
 
-      // Apply 1/2 factor and symmetry term
-      elmat_p.Symmetrize();
+      // Symmetrize the jump term
+      elmatJ_p.Symmetrize();
+      if (!ndof[1])
+      {
+         elmatJ_p *= 2;
+      }
 
-      elmat_p *= ip.weight * Trans.Weight();
-
-      elmat += elmat_p;
+      // Then just add penalty
+      elmatJ_p *= -1;
+      elmatJ_p += elmatC_p;
+      elmatJ_p *= ip.weight * Trans.Weight();
+      elmat += elmatJ_p;
    }
 }
